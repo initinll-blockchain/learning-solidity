3352668780f51fac00ee533c29e530a1af1d2035
Nitin Londhe <nitin.londhe@genmills.com>
2022-01-14 01:24:44 +0530

    added 35.Interface.sol

---
+++ 35.Interface.sol
@@ -0,0 +1,36 @@
+//SPDX-Licence-Identifier: UNLICENCED"
+
+pragma solidity 0.8.11;
+
+/*    
+    Interface - 
+       - cannot have any functions implemented
+       - can inherit from other interfaces
+       - all declared functions must be external
+       - cannot declare a constructor
+       - cannot declare state variables
+*/
+
+contract Counter {
+    uint public count;
+
+    function increment() external {
+        count += 1;
+    }
+}
+
+interface ICounter {
+    function count() external view returns (uint);
+
+    function increment() external;
+}
+
+contract MyContract {
+    function incrementCounter(address _counter) external {
+        ICounter(_counter).increment();
+    }
+
+    function getCount(address _counter) external view returns (uint) {
+        return ICounter(_counter).count();
+    }
+}
